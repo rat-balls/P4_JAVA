baa1a9e96768b4678022b991659cde0d87b52adc
SiERRA74 <aksierra74@gmail.com>
2023-01-23 11:30:44 +0100

    setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class App {
+
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("[1] jouer à 2 \n[2]jouer avec une IA");
+        String str = sc.nextLine();
+        
+
     }
 }
