a5575b499a52d8ec1d844f87727ae2d60010b345
SiERRA74 <aksierra74@gmail.com>
2023-01-23 15:51:18 +0100

    Squashed commit of the following:
    commit c931e87a4ffaa2ba11b981eef62868054b4c5513
    Author: ethan2 <116553314+rat-balls@users.noreply.github.com>
    Date:   Mon Jan 23 15:43:24 2023 +0100
    
        moved grid generator into grid.java
    
    commit 357f7fb443fb2f94e636975813408a71818fa3ac
    Author: ethan2 <116553314+rat-balls@users.noreply.github.com>
    Date:   Mon Jan 23 15:37:28 2023 +0100
    
        start of grid
    
    commit 3e57a32c2d2a51c9d8634d7fe063f6eacbe563d5
    Author: ethan2 <116553314+rat-balls@users.noreply.github.com>
    Date:   Mon Jan 23 15:30:47 2023 +0100
    
        fixed array lists in classes
    
    commit b73ce826729091125d0cfedb4b14266b17721cb3
    Author: ethan2 <116553314+rat-balls@users.noreply.github.com>
    Date:   Mon Jan 23 15:14:48 2023 +0100
    
        privated stuff
    
    commit 79cb5b19712b9d55d95363a5b57c1298be666c79
    Author: ethan2 <116553314+rat-balls@users.noreply.github.com>
    Date:   Mon Jan 23 15:11:00 2023 +0100
    
        added classes
    
    commit e87c8791145c5fcc5c3e1b5918b9672a4468e1df
    Merge: e3d6743 baa1a9e
    Author: ethan2 <116553314+rat-balls@users.noreply.github.com>
    Date:   Mon Jan 23 14:51:06 2023 +0100
    
        Merge branch 'main' into ethan
    
    commit e3d67438ee93d1aa221fb539d6e6268f335f2c61
    Author: ethan2 <116553314+rat-balls@users.noreply.github.com>
    Date:   Mon Jan 23 14:16:04 2023 +0100
    
        Update .gitignore
    
    commit baa1a9e96768b4678022b991659cde0d87b52adc
    Author: SiERRA74 <aksierra74@gmail.com>
    Date:   Mon Jan 23 11:30:44 2023 +0100
    
        setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import model.*;
+
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
+
 }
