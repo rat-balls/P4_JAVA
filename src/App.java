601c2af25bab6c159e1e8fa20a601b4b39b20da8
Sacha Harel <sacha.harel@gmail.com>
2023-01-23 15:25:04 +0100

    Commit menu

--- src/App.java
+++ src/App.java
@@ -1,13 +1,67 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-
-    
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("[1] jouer à 2 \n[2]jouer avec une IA");
-        String str = sc.nextLine();
-        
-
+        while(true) {
+            afficherMenu();
+            String choix = _scan.nextLine();
+            switch(choix) {
+                case "1":
+                    jouer();
+                    break;
+                case "2":
+                    System.out.println("Paramètres");
+                    break;
+                case "3":
+                    System.out.println("Quitter");
+                    return;
+                default:
+                    System.out.println("Aucune option correspondante. Veuillez réessayer.");
+                    break;
+            }
+        }
     }
+
+
+    // Affiche le menu ====================
+    // ====================================
+    public static void afficherMenu() {
+        ArrayList<String> menus = new ArrayList<>();
+        menus.add("\n-- MENU PUISSANCE 4 --");
+        menus.add("[1] Jouer");
+        menus.add("[2] Paramètres");
+        menus.add("[3] Quitter");
+        for (String menu : menus) {
+            System.out.println(menu);
+        }
+    }
+    // ====================================
+    // ====================================
+
+
+    // Lance le menu Jouer ================
+    // ====================================
+    public static void jouer() {
+        System.out.println("[1] Contre l'Ordi\n[2] Contre un ami\n[3] Retour");
+        String choix = _scan.nextLine();
+        switch(choix) {
+            case "1":
+                System.out.println("Jouer contre l'Ordi");
+                break;
+            case "2":
+                System.out.println("Jouer contre un ami");
+                break;
+            case "3":
+                System.out.println("Quitter");
+                return;
+            default:
+                System.out.println("Aucune option correspondante. Veuillez réessayer.");
+                break;
+        }
+    }
+    // ====================================
+    // ====================================
+
+    private static Scanner _scan = new Scanner(System.in);
 }
