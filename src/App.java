196b7f03cf21f954f7d3f805e66d365d4f747cfa
SiERRA74 <aksierra74@gmail.com>
2023-01-24 11:51:13 +0100

    rien

--- src/App.java
+++ src/App.java
@@ -3,17 +3,7 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Grid grid = new Grid();
-        System.out.println();
-        System.out.println();
-        ArrayList g = grid.createGrid();
-        for(int iii = 0; iii < 20; iii++){
-            grid.affichage(g);
-            System.out.println();
-            g = grid.modif( g, "Entrer le numéro de la colone");
-            System.out.print(g);
-            grid.affichage(g);
-        }
+        
         
     }
 
