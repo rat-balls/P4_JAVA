94c677743da60dae3b3add35e3e938aa7c9fe2c5
ethan2 <116553314+rat-balls@users.noreply.github.com>
2023-01-24 10:16:18 +0100

    Update Game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,15 +1,22 @@
 package model;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     
     public static void startGame(){
         System.out.println();
         System.out.println();
+
+        
         ArrayList<ArrayList<String>> g = Grid.createGrid();
         Grid.affichage(g);
         System.out.println();
         System.out.println();
     }
 
+    private static Scanner _scan = new Scanner(System.in);
+
+
 }
+
