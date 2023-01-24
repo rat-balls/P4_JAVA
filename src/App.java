dbdb984644ebbf908b300a647d2083ceb90c084b
ethan2 <116553314+rat-balls@users.noreply.github.com>
2023-01-24 10:18:10 +0100

    Update App.java

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import model.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+        System.out.println();
+        ArrayList<ArrayList<String>> g = Grid.createGrid();
+        Grid.affichage(g);
+        System.out.println();
+        System.out.println();
+
+        Scanner s = new Scanner(System.in);
+        
+    }
+
+}
