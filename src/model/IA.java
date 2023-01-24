dbf7e69cb223a533ce25996a829ee3f0e4c2a009
Tristan Turgy <116552625+ArKZbeb@users.noreply.github.com>
2023-01-24 11:11:21 +0100

    fix IA

--- src/model/IA.java
+++ src/model/IA.java
@@ -1,24 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+
+
 public class IA {
     
-    private Joueur Joueur;
-    private int Difficulté;
+    private Joueur joueur;
+    private int difficulte;
 
-    public IA(model.Joueur joueur, int difficulté) {
-        Joueur = joueur;
-        Difficulté = difficulté;
+    public IA(model.Joueur joueur, int difficulte) {
+        this.joueur = joueur;
+        this.difficulte = difficulte;
     }
     public Joueur getJoueur() {
-        return Joueur;
+        return joueur;
     }
     public void setJoueur(Joueur joueur) {
-        Joueur = joueur;
+        this.joueur = joueur;
     }
-    public int getDifficulté() {
-        return Difficulté;
+    public int getDifficulte() {
+        return difficulte;
     }
-    public void setDifficulté(int difficulté) {
-        Difficulté = difficulté;
+    public void setDifficulte(int difficulte) {
+        this.difficulte = difficulte;
+    }
+    // ============================================================================================
+    // ============================================================================================
+    public static void jeuOrdi(){
+        System.out.println();
+        System.out.println();
+        ArrayList<ArrayList<String>> g = Grid.createGrid();
+        Grid.affichage(g);
+        System.out.println();
+        System.out.println();
     }
 }
