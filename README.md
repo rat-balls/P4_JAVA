277ee60096cfe7eeeb8678a5a18216abc0216818
Tristan <turgytristan@gmail.com>
2023-01-29 17:27:56 +0100

    fix grid + doc tristan

--- README.md
+++ README.md
@@ -41,3 +41,19 @@ Cette fonction prends en paramètre la liste que créer la fonction plus haut, e
 Pour apporter des modifications à la grille Amin écrit les bases d'une modifications (ajout de pions) qui vont être réutilisé à plusieurs réprises dans d'autres Classes. <br>
 `modif(ArrayList<ArrayList<String>> g, String message, String symbole)`<br>
 Comme pour `affichage()`, `modif()`prends en paramètres : une grille. Mais aussi un message et un symbole, le message correspond au message d'erreur en cas de mauvais input (pas un chiffre, chiffre négatif, chiffre trop grand), et symbole qui correspond à l'apparence du pion. La fonction par cours la grille avec 2 boucles for, remplace libre de la colone designer par le jour avec son symbole.
+
+## IA
+
+Tristan c'est ensuite occupé de la partie IA, en reprenant la méthode `modif` il a au début fait une autre méthode `IAmodif` qui prends en paramètre `ArrayList<ArrayList<String>> g, String symbole` ce qui est exactement la même chose que pour `modif` sauf que le message a été retiré car l'IA au niveau 1 cherche seulement une colonne aléatoire pour placer son pion.
+
+Par la suite il a fait l'IA niveau 2, il nous était demander de bloquer le prochain coup du joueur si celui-ci lui permet de gagner ainsi il a fait une méthode `anticipation(ArrayList<ArrayList<String>> g, String symbole, String symbole2)` ici symbole est le symbole du joueur et symbole2 est celui de l'IA. Cette méthode est ici pour parcourir le tableau et regarde si 3 symboles du joueur sont alignés, si c'est le cas alors elle renvoie la colone du point à jouer. Ensuite la méthode `IAmodif2` a été crée, c'est la même chose que pour `IAmodif` sauf qu'il y a une condition pour chosir si il doit bloquer le joueur de jouer ou jouer un pion aléatoirement.
+
+Ensuite il a fait une méthode `checkWin` pour verifier si quelqu'un a gagner et si oui elle retourne true ce qui arrete le jeu tout en métant un message pour savoir qui a gagner
+
+La méthode permettant de faire l'ia niveau 3 a été faite par Tristan et Éthan, elle marche comme pour celle de l'ia niveau 2 mais elle est un peu plus poussé, si trois pion sont peu importe l'ordre, et comment elle va les bloquer et elle regarde seulement si la ligne est supérieur ou égal à 1 alors elle regarde la ligne du dessous 3 colonnes plus loin en avant ou en arrière si elle est diffèrentes de "-" qui ici représente une case vide, si elle est vide elle ne va pas placer le pion ici mais autre part.
+
+Pour finir on à la méthode `Anticipationniv3` qui est un copier collé de l'anticipation niv2 sauf que certaines conditions sont supprimées et qu'on inverse les 2 symboles, pour ne pas vérifier si le joueur peut gagner mais l'ia et retourne la colonne à jouer.
+
+Enfin nous avons la méthode `checkWinIA` qui est la même chose que le checkWin mais pour l'ia et elle retourne true ou false
+
+Par la suite nous avons mis dans IA.java la méthode `IAmodif4` qui est la méthode appelé pour jouer le coup de l'ia, si le `checkWinIA` est vrai alors la méthode le coup jouer sera le résultat de l'Anticipationniv3 sinon si le résultat de la méthode `anticipationniv2` est différent de -1 alors la colonne jouer est le résultat de anticipation niv2 sinon c‘est une colonne aléatoire.
--- src/model/Grid.java
+++ src/model/Grid.java
@@ -179,109 +179,7 @@ public class Grid {
         return -1;
     }
 
-    public static int anticipationNiv2(ArrayList<ArrayList<String>> g, String symbole, String symbole2){
-        // check horizontal
-        System.out.println("check horizontal");
-        for (int li = 0; li < 6; li++){
-            for (int col = 0; col < 4; col++){
-                if (li == 0){
-                    if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+2).equals(symbole) && !g.get(li).get(col+3).equals(symbole2) && !g.get(li).get(col+3).equals("-")){
-                        System.out.println("horz 1");
-                        return col+3;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+2).equals(symbole2) && !g.get(li).get(col+2).equals("-")){
-                        System.out.println("horz 2");
-                        return col+2;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+2).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+1).equals(symbole2) && !g.get(li).get(col+1).equals("-")){
-                        System.out.println("horz 3");
-                        return col+1;
-                    }
-                } else if (col < 3) {
-                    if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+2).equals(symbole) && !g.get(li).get(col+3).equals(symbole2) && !g.get(li-1).get(col+3).equals("-")){
-                        System.out.println("horz 2 1");
-                        return col+3;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+2).equals(symbole2) && !g.get(li-1).get(col+2).equals("-")){
-                        System.out.println("horz 2 2");
-                        return col+2;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+2).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+1).equals(symbole2) && !g.get(li-1).get(col+1).equals("-")){
-                        System.out.println("horz 2 3");
-                        return col+1;
-                    }
-                } else {
-                    if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+2).equals(symbole) && !g.get(li).get(col+3).equals(symbole2) && !g.get(li-1).get(col+3).equals("-")){
-                        System.out.println("horz 2 1");
-                        return col+3;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+2).equals(symbole2) && !g.get(li-1).get(col+2).equals("-")){
-                        System.out.println("horz 2 2");
-                        return col+2;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+2).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+1).equals(symbole2) && !g.get(li-1).get(col+1).equals("-")){
-                        System.out.println("horz 2 3");
-                        return col+1;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col-1).equals(symbole) && g.get(li).get(col-2).equals(symbole) && !g.get(li).get(col-3).equals(symbole2) && !g.get(li-1).get(col-3).equals("-")){
-                        System.out.println("horz 2 4");
-                        return col-3;
-                    }
-                }
-            }
-        }
-
-        // check vertical
-        System.out.println("check vertical");
-
-        for (int li = 0; li < 3; li++) {
-            for (int col = 0; col < 7; col++) {
-                if (g.get(li).get(col).equals(symbole) && g.get(li+1).get(col).equals(symbole) && g.get(li+2).get(col).equals(symbole) && !g.get(li+3).get(col).equals(symbole2)){
-                    if (g.get(li).get(col).equals(symbole) && g.get(li+1).get(col).equals(symbole) && g.get(li+2).get(col).equals(symbole)){
-                        return col;
-                    }
-                }
-            }
-        }
-        // check diagonal
-        System.out.println("check diagonal");
-
-        for (int li = 0; li < 3; li++) {
-            for (int col = 0; col < 4; col++) {
-                if (g.get(li).get(col).equals(symbole) && g.get(li+1).get(col+1).equals(symbole) && g.get(li+2).get(col+2).equals(symbole) && !g.get(li+3).get(col+3).equals(symbole2) && !g.get(li+2).get(col+3).equals("-")){
-                    return col+3;
-                } else if (g.get(li).get(col).equals(symbole) && g.get(li+1).get(col+1).equals(symbole) && g.get(li+3).get(col+3).equals(symbole) && !g.get(li+2).get(col+2).equals(symbole2) && !g.get(li+1).get(col+2).equals("-")){
-                    return col+2;
-                } else if (g.get(li).get(col).equals(symbole) && g.get(li+2).get(col+2).equals(symbole) && g.get(li+3).get(col+3).equals(symbole) && !g.get(li+1).get(col+1).equals(symbole2) && !g.get(li).get(col+1).equals("-")){
-                    return col+1;
-                }
-            }
-        }
-        // check anti-diagonal
-        System.out.println("check anti-diagonal");
-
-        for (int li = 0; li < 3; li++) {
-            for (int col = 3; col < 7; col++) {
-                if(li >= 4){
-                    if (g.get(li).get(col).equals(symbole) && g.get(li-1).get(col+1).equals(symbole) && g.get(li-2).get(col+2).equals(symbole) && !g.get(li-3).get(col+3).equals(symbole2) && !g.get(li-4).get(col+3).equals("-")){
-                        System.out.println("antidiag 1");
-                        return col+3;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li-1).get(col+1).equals(symbole) && g.get(li-3).get(col+3).equals(symbole) && !g.get(li-2).get(col+2).equals(symbole2) && !g.get(li-3).get(col+2).equals("-")){
-                        System.out.println("antidiag 2");
-                        return col+2;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li-2).get(col+2).equals(symbole) && g.get(li-3).get(col+3).equals(symbole) && !g.get(li-1).get(col+1).equals(symbole2) && !g.get(li-2).get(col+1).equals("-")){
-                        System.out.println("antidiag 3");
-                        return col+1;
-                    }
-                } else if(li == 3){
-                    if (g.get(li).get(col).equals(symbole) && g.get(li-1).get(col+1).equals(symbole) && g.get(li-2).get(col+2).equals(symbole) && !g.get(li-3).get(col+3).equals(symbole2)){
-                        System.out.println("antidiag 1");
-                        return col+3;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li-1).get(col+1).equals(symbole) && g.get(li-3).get(col+3).equals(symbole) && !g.get(li-2).get(col+2).equals(symbole2) && !g.get(li-3).get(col+2).equals("-")){
-                        System.out.println("antidiag 2");
-                        return col+2;
-                    } else if (g.get(li).get(col).equals(symbole) && g.get(li-2).get(col+2).equals(symbole) && g.get(li-3).get(col+3).equals(symbole) && !g.get(li-1).get(col+1).equals(symbole2) && !g.get(li-2).get(col+1).equals("-")){
-                        System.out.println("antidiag 3");
-                        return col+1;
-                    }
-                } 
-            }
-        }
-        return -1;    
-    }
+    
 
     public static boolean checkWinIA(ArrayList<ArrayList<String>> g, String symboleIA){
         // check horizontal
@@ -332,20 +230,20 @@ public class Grid {
 
     
     
-        public static int anticipationNiv2(ArrayList<ArrayList<String>> g, String symbole, String symbole2){
-            // check horizontal
-            System.out.println("check horizontal");
-            for (int li = 0; li < 6; li++){
-                for (int col = 0; col < 4; col++){
-                    if (li == 0){
-                        System.out.println("test1");
-                        if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+2).equals(symbole) && !g.get(li).get(col+3).equals(symbole2) && !g.get(li).get(col+3).equals("-")){
-                            System.out.println("horz 1");
-                            return col+3;
-                        } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+2).equals(symbole2) && !g.get(li).get(col+2).equals("-")){
-                            System.out.println("horz 2");
-                            return col+2;
-                        } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+2).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+1).equals(symbole2) && !g.get(li).get(col+1).equals("-")){
+    public static int anticipationNiv2(ArrayList<ArrayList<String>> g, String symbole, String symbole2){
+        // check horizontal
+        System.out.println("check horizontal");
+        for (int li = 0; li < 6; li++){
+            for (int col = 0; col < 4; col++){
+                if (li == 0){
+                    System.out.println("test1");
+                    if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+2).equals(symbole) && !g.get(li).get(col+3).equals(symbole2) && !g.get(li).get(col+3).equals("-")){
+                        System.out.println("horz 1");
+                        return col+3;
+                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+1).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+2).equals(symbole2) && !g.get(li).get(col+2).equals("-")){
+                        System.out.println("horz 2");
+                        return col+2;
+                    } else if (g.get(li).get(col).equals(symbole) && g.get(li).get(col+2).equals(symbole) && g.get(li).get(col+3).equals(symbole) && !g.get(li).get(col+1).equals(symbole2) && !g.get(li).get(col+1).equals("-")){
                             System.out.println("horz 3");
                             return col+1;
                         }
